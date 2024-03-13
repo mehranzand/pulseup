7c3973587a18c14634fe2c05c4951dfa9f7305cd
Mehran Zand <mehran.zand@gmail.com>
2024-03-13 16:35:52 +0330

    build up struc

--- go.mod
+++ go.mod
@@ -5,11 +5,31 @@ go 1.22.1
 require github.com/labstack/echo/v4 v4.11.4
 
 require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/alexflint/go-arg v1.4.3 // indirect
+	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/distribution/reference v0.5.0 // indirect
+	github.com/docker/docker v25.0.4+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
+	go.opentelemetry.io/otel v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- main.go
+++ main.go
@@ -2,17 +2,86 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/alexflint/go-arg"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/mehranzand/pulseup/web"
+	log "github.com/sirupsen/logrus"
 )
 
+var (
+	version = "head"
+)
+
+type args struct {
+	Addr          string              `arg:"env:PULSEUP_ADDR" default:":7070" help:"sets host:port to bind for server. This is rarely needed inside a docker container."`
+	Base          string              `arg:"env:PULSEUP_BASE" default:"/" help:"sets the base for http router."`
+	Hostname      string              `arg:"env:PULSEUP_HOSTNAME" help:"sets the hostname for display. This is useful with multiple pulseUp instances."`
+	Level         string              `arg:"env:PULSEUP_LEVEL" default:"info" help:"set pulseUp log level. Use debug for more logging."`
+	Username      string              `arg:"env:PULSEUP_USERNAME" help:"sets the username for auth."`
+	Password      string              `arg:"env:PULSEUP_PASSWORD" help:"sets password for auth"`
+	FilterStrings []string            `arg:"env:PULSEUP_FILTER,--filter,separate" help:"filters docker containers using Docker syntax."`
+	Filter        map[string][]string `arg:"-"`
+}
+
+func (args) Version() string {
+	return version
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	args := parseArgs()
+	if len(args.Addr) == 0 {
+		log.Fatal("PULSEUP_ADDR can't be null or empty")
+	}
+
+	log.Infof("pulseUp version %s", version)
+
 	e := echo.New()
 	web.RegisterHandlers(e)
 	e.GET("/api", func(c echo.Context) error {
 		return c.String(http.StatusOK, "pulseUp")
 	})
 
-	e.Logger.Fatal(e.Start(":7070"))
+	e.Logger.Fatal(e.Start(args.Addr))
+}
+
+func parseArgs() args {
+	var args args
+	parser := arg.MustParse(&args)
+
+	configureLogger(args.Level)
+
+	args.Filter = make(map[string][]string)
+
+	for _, filter := range args.FilterStrings {
+		pos := strings.Index(filter, "=")
+		if pos == -1 {
+			parser.Fail("each filter should be of the form key=value")
+		}
+		key := filter[:pos]
+		val := filter[pos+1:]
+		args.Filter[key] = append(args.Filter[key], val)
+	}
+
+	return args
+}
+
+func configureLogger(level string) {
+	if l, err := log.ParseLevel(level); err == nil {
+		log.SetLevel(l)
+	} else {
+		panic(err)
+	}
+
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+		FullTimestamp: true,
+	})
 }
