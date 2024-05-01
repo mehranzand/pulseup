02d1e78ba6fd571d7c1be232fce74acbac5f9282
Mehran Zand <mehran.zand@gmail.com>
2024-05-01 11:25:09 +0330

    chore: pass config to web app

---
+++ web/src/stores/config.ts
@@ -0,0 +1,20 @@
+
+const content = document.querySelector("script#pulseup__config")?.textContent || "{}";
+
+export interface Config {
+    hostname: string;
+    version: string;
+    base: string;
+    hosts: { name: string; id: string }[];
+    authProvider: "simple" | "none" ;
+  }
+  
+  const jsonConfig = JSON.parse(content);
+
+  const config: Config = {
+    ...jsonConfig,
+  };
+  
+  export default Object.freeze(config);
+
+  export const withBase = (path: string) => `${config.base}${path}`;
