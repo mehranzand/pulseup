d6b59e1ac179e03e711298957cee809232a33c86
Mehran Zand <mehran.zand@gmail.com>
2024-11-12 16:38:27 +0330

    chore: some ui improvements

--- web/src/layout/Header.tsx
+++ web/src/layout/Header.tsx
@@ -1,9 +1,10 @@
 import { Layout as AntLayout, Space } from "antd";
-import  { DockerOutlined } from '@ant-design/icons';
+import { DockerOutlined } from '@ant-design/icons';
+import './header.css';
 
 function Header() {
   return (
-    <AntLayout.Header style={{backgroundColor: "#099cec", height: 40, color: "#fff", fontSize: 23, lineHeight:"35px", fontFamily:"monospace", fontWeight: 700}}>
+    <AntLayout.Header className="header" >
       <Space>
         <DockerOutlined />
         <span>pulseUp</span>
